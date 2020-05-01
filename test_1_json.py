1629ab4699b78e18b372dd12f66d07ec7addd786
David Test <davjimen1@gmail.com>
2020-05-01 13:05:40 +0200

    this only a test

--- test_1_json.py
+++ test_1_json.py
@@ -6,7 +6,7 @@ user = data['user']
 print(user['name'])
 for role in user['roles']:
     print(role)
-
+# this is a test
 user['location']['city'] = 'Barcelona' # change the diccionary
 with open('json_example-edited.json', 'w') as file:
     json.dump(user, file, indent=4)  # load the new value into json format
